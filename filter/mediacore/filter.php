eecc07c9c257b5eba309a3b80dce21cc9cb97396
Anthony Theocharis <anthony.theocharis@gmail.com>
2014-05-27 17:08:54 -0700

    squash me onto ocd commit

--- filter/mediacore/filter.php
+++ filter/mediacore/filter.php
@@ -27,6 +27,7 @@
  * @subpackage mediacore
  * @copyright  2012 MediaCore Technologies
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
  */
 
 defined('MOODLE_INTERNAL') || die('Invalid access');
