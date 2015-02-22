877bc04b9729a90cd0b1a457a602285697f5674d
Derek Harnanansingh <derek@mediacore.com>
2015-02-22 01:30:34 -0800

    Refactor filter code
    Added support for checking for regular embed urls
    or trusted embed urls.
    Split out image width and height calculations so
    it can be used across multiple functions.
    Split out iframe html creation so it can be used
    across multiple functions.

--- filter/mediacore/filter.php
+++ filter/mediacore/filter.php
@@ -45,8 +45,8 @@ require_once('mediacore_client.class.php');
 class filter_mediacore extends moodle_text_filter {
 
     private $_mcore_client;
-    private $_api1_view_link_re;
-    private $_api2_view_link_re;
+    private $_api1_public_url_re;
+    private $_api2_public_url_re;
     private $_default_thumb_width = 400;
     private $_default_thumb_height = 225;
 
@@ -59,17 +59,19 @@ class filter_mediacore extends moodle_text_filter {
         parent::__construct($context, $localconfig);
         $this->_mcore_client = new mediacore_client();
         $host = $this->_mcore_client->get_host();
-        $this->_api1_view_link_re = "/($host)[:0-9]*\/media\/[:a-z0-9_-]+/";
-        $this->_api2_view_link_re = "/($host)[:0-9]*\/api2\/media\/[0-9]+\/view/";
+        $this->_trusted_embed_url_re = "/($host)[:0-9]*\/media\/.*embed_token=.*/";
+        $this->_embed_url_re = "/($host)[:0-9]*\/media\/.*embed_player.*/";
+        $this->_api1_public_url_re = "/($host)[:0-9]*\/media\/[:a-z0-9_-]+/";
+        $this->_api2_public_url_re = "/($host)[:0-9]*\/api2\/media\/[0-9]+\/view/";
     }
 
     /**
      * Filter the page html and look for an <a><img> element added by the chooser
      * or an <a> element added by the moodle file picker
      * NOTE: An embed from the Chooser and a link from the old filepicker are
-     *   of the same form (see $_api1_view_link_re).
+     *   of the same form (see $_api1_public_url_re).
      * A link from the new repository filepicker plugin is different
-     *   (see $_api2_view_link_re).
+     *   (see $_api2_public_url_re).
      * The Chooser style link will updated in the future to match the new link
      *   style.
      *
@@ -93,28 +95,25 @@ class filter_mediacore extends moodle_text_filter {
             if (empty($href)) {
                 continue;
             }
-            if ((boolean)preg_match($this->_api1_view_link_re, $href)) {
+            if ((boolean)preg_match($this->_trusted_embed_url_re, $href) ||
+                (boolean)preg_match($this->_embed_url_re, $href)) {
+                $href = str_replace('&', '&amp;', $href);
                 $newnode  = $dom->createDocumentFragment();
                 $imgnode = $node->firstChild;
-
-                if ($imgnode && $imgnode instanceof DOMElement) {
-                    $width = $imgnode->getAttribute('width');
-                    $height = $imgnode->getAttribute('height');
-                }
-
-                if (empty($width) || empty($height)
-                    || ($width == 195 && $height == 110)) {
-                    // Keep old moodle embeds @ the default size
-                    $width = $this->_default_thumb_width;
-                    $height = $this->_default_thumb_height;
-                }
-
+                extract($this->_get_image_elem_dimensions($imgnode));
+                $html = $this->_get_iframe_embed_html($href, $width, $height);
+                $newnode->appendXML($html);
+                $node->parentNode->replaceChild($newnode, $node);
+            } else if ((boolean)preg_match($this->_api1_public_url_re, $href)) {
+                $newnode  = $dom->createDocumentFragment();
+                $imgnode = $node->firstChild;
+                extract($this->_get_image_elem_dimensions($imgnode));
                 $html = $this->_get_embed_html_from_api1_view_url(
                     $href, $width, $height, $courseid);
                 $newnode->appendXML($html);
                 $node->parentNode->replaceChild($newnode, $node);
 
-            } else if ((boolean)preg_match($this->_api2_view_link_re, $href)) {
+            } else if ((boolean)preg_match($this->_api2_public_url_re, $href)) {
                 $newnode  = $dom->createDocumentFragment();
                 $width = $this->_default_thumb_width;
                 $height = $this->_default_thumb_height;
@@ -125,6 +124,26 @@ class filter_mediacore extends moodle_text_filter {
             }
         }
         return $dom->saveHTML();
+    }
+
+    /**
+     * Fetch the width an height from an image element
+     */
+    private function _get_image_elem_dimensions($imgnode) {
+        if ($imgnode && $imgnode instanceof DOMElement) {
+            $width = $imgnode->getAttribute('width');
+            $height = $imgnode->getAttribute('height');
+        }
+        if (empty($width) || empty($height)
+            || ($width == 195 && $height == 110)) {
+            // Keep old moodle embeds @ the default size
+            $width = $this->_default_thumb_width;
+            $height = $this->_default_thumb_height;
+        }
+        return array(
+            'width' => $width,
+            'height' => $height
+        );
     }
 
     /**
@@ -189,6 +208,14 @@ class filter_mediacore extends moodle_text_filter {
             //return $this->_get_embed_error_html($error='Unexpected Json:' . $result);
         //}
         //$template = $json->html;
+        return $this->_get_iframe_embed_html($embed_url, $width, $height);
+    }
+
+    /**
+     * Get the iframe embed html
+     * @return string
+     */
+    private function _get_iframe_embed_html($embed_url, $width, $height) {
         $template = '<iframe src="URL" ' .
             'width="WIDTH" ' .
             'height="HEIGHT" ' .
@@ -196,7 +223,6 @@ class filter_mediacore extends moodle_text_filter {
             'allowfullscreen="allowfullscreen" ' .
             'frameborder="0"> ' .
             '</iframe>';
-
         $patterns = array('/URL/', '/WIDTH/', '/HEIGHT/');
         $replace = array($embed_url, $width, $height);
         return preg_replace($patterns, $replace, $template);
