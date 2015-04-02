dae72aca693d6fe6ca1aa0d92f536a99b9db70d2
Derek Harnanansingh <derek@mediacore.com>
2015-04-02 11:01:21 -0700

    Update signing logic
    Use siteurl so we have config defined scheme/hostname:port
    Throw errors if anything invalid is found

--- filter/mediacore/sign.php
+++ filter/mediacore/sign.php
@@ -4,21 +4,26 @@ require_once $CFG->dirroot . '/local/mediacore/lib.php';
 require_once('mediacore_client.class.php');
 
 $url = $_SERVER['REQUEST_URI'];
+$mcore_client = new mediacore_client();
 
 $pos = strpos($url, '?');
-if ($pos !== false) {
-    // The url contains query params, so split out the query string
-    // params as an array so we can pass them to the lti signing
-    // method
-    $qs = substr($url, $pos + 1);
-    $params = array();
-    parse_str($qs, $params);
-    $url = substr($url, 0, $pos);
+if ($pos === false) {
+    $response_string = 'HTTP/1.1 400 Bad Request';
+    header($response_string);
+    echo $response_string;
+    return;
 }
-$courseid = $params['context_id'];
+// The url contains query params, so split out the query string
+// params as an array so we can pass them to the lti signing
+// method
+$qs = substr($url, $pos + 1);
+$params = array();
+parse_str($qs, $params);
+$url = substr($url, 0, $pos);
 
-$mcore_client = new mediacore_client();
-$url = str_replace($_SERVER['SCRIPT_NAME'], 'https://'.$mcore_client->get_host(), $url);
+$courseid = $params['context_id'];
+$site_url = $mcore_client->get_siteurl();
+$url = str_replace($_SERVER['SCRIPT_NAME'], $site_url, $url);
 
 $params = $mcore_client->get_signed_lti_params(
     $url, 'GET', $courseid, $params
