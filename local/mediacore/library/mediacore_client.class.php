b46df43fcafbc23b8467b840ced95c9ceeb1f04d
Derek Harnanansingh <derek@mediacore.com>
2015-08-25 09:29:30 -0700

    Ensure moodle releases < 2.8 does not pass the lti2 arg to lti_get_ims_role

--- local/mediacore/library/mediacore_client.class.php
+++ local/mediacore/library/mediacore_client.class.php
@@ -348,11 +348,9 @@ class mediacore_client
         $user_full = trim($user_given . ' ' . $user_family);
         $user_email = (isset($USER->email)) ? $USER->email: '';
 
-        if (strpos($CFG->release, '2.8') === false) {
+        if (floatval($CFG->release) < 2.8) {
             $roles = lti_get_ims_role($USER, 0, $course->id);
         } else {
-            // NOTE: Moodle 2.8 adds support for specifying whether this is
-            //       an LTI 2.0 launch.
             $roles = lti_get_ims_role($USER, 0, $course->id, false);
         }
 
