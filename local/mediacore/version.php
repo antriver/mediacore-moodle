4683a602862cfbf0649bb7c717a37befbccf95c6
Derek Harnanansingh <derek.harnanansingh@workday.com>
2015-09-22 13:45:19 -0700

    Update local version to 2015092200

--- local/mediacore/version.php
+++ local/mediacore/version.php
@@ -34,9 +34,9 @@ defined('MOODLE_INTERNAL') || die('Invalid access');
 
 $plugin                     = new stdClass();
 $plugin->component          = 'local_mediacore';
-$plugin->version            = 2015060800;
+$plugin->version            = 2015092200;
 $plugin->requires           = 2012062500;
-$plugin->release            = '3.0.8';
+$plugin->release            = '3.0.9';
 $plugin->maturity           = MATURITY_STABLE;
 $plugin->dependencies       = array(
     'mod_lti' => 2011112900,
